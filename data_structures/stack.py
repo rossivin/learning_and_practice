ad4ea6cf97c442f412ca8a1d080b108e2e2ff6c7
Vinicius Rossi <rossi.vinicius@outlook.com>
2021-05-25 13:52:38 -0400

    adding the basic code for stacks, queues, and dequeues

---
+++ data_structures/stack.py
@@ -0,0 +1,21 @@
+#PUSHING AND PULLING FROM THE STACK ARE O(1) OPERATIONS
+
+class Stack:
+
+	def __init__(self):
+		self.items = []
+
+	def isEmpty(self):
+		return self.items == []
+
+	def push(self, item):
+		self.items.append(item)
+
+	def pop(self):
+		return self.items.pop()
+
+	def peek(self):
+		return self.items[len(self.items)-1]
+
+	def size(self):
+		return len(self.items)
